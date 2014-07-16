293fb942eb3830768056a5b0ab3365ed2bf8fe82
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-07-16 02:37:00 +0000

    ADDING: fb update task
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/sharethis/trunk@5751 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/api/FacebookFeed.php
+++ code/api/FacebookFeed.php
@@ -254,3 +254,25 @@ class FacebookFeed_Item_Communicator extends Object {
 
 
 }
+
+
+
+class FacebookFeed_UpdateTask extends BuildTask {
+
+	protected $title = "Update Facebook News";
+
+	protected $description = "Checks for updates on Facebook";
+
+	/**
+	 *
+	 */
+	function run($request) {
+		$facebookPages = DataObject::get('FacebookFeed_Page');
+		if($facebookPages && $facebookPages->Count()) {
+			foreach($facebookPages as $facebookPage) {
+				DB::alteration_message("Facebook page #{$facebookPage->ID} '$facebookPage->Title' updated", 'changed');
+				$facebookPage->Fetch();
+			}
+		}
+	}
+}
