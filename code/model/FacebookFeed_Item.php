26ffb0ccee578d46b0151f8eacda21d0f4174fbc
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-08-24 09:25:41 +0000

    PHP upgrade
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/sharethis/trunk@5785 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/model/FacebookFeed_Item.php
+++ code/model/FacebookFeed_Item.php
@@ -97,10 +97,17 @@ class FacebookFeed_Item extends DataObject{
 		foreach($html->find('text') as $element) {
 			//what exactly does it do?
 			if(! in_array($element->parent()->tag, array('a', 'img'))) {
-				$element->innertext = preg_replace("#(www(\S*?\.\S*?))(\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)#ie", "'http://$1$4'", $element->innertext);
-				$element->innertext = preg_replace("#((http|https|ftp)://(\S*?\.\S*?))(\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)#ie", "'<a href=\"$1\" target=\"_blank\">click here</a>$4'", $element->innertext);
+				if(phpversion() < 5.4) {
+					//to do - what does this do and how does it need to be written????
+					$element->innertext = preg_replace_callback("#(www(\S*?\.\S*?))(\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)#ie", "'http://$1$4'", $element->innertext);
+					$element->innertext = preg_replace_callback("#((http|https|ftp)://(\S*?\.\S*?))(\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)#ie", "'<a href=\"$1\" target=\"_blank\">click here</a>$4'", $element->innertext);
+				}
+				else {
+					$element->innertext = $this->replaceLinksWithProperOnes($element->innertext);
+				}
 			}
 		}
+
 		$dom = new DOMDocument();
 		@$dom->loadHTML($html);
 		if($dom) {
@@ -114,6 +121,53 @@ class FacebookFeed_Item extends DataObject{
 			}
 		}
 		return $dom->saveHTML();
+	}
+
+	protected function replaceLinksWithProperOnes($text) {
+		$rexProtocol = '(https?://)?';
+		$rexDomain   = '((?:[-a-zA-Z0-9]{1,63}\.)+[-a-zA-Z0-9]{2,63}|(?:[0-9]{1,3}\.){3}[0-9]{1,3})';
+		$rexPort     = '(:[0-9]{1,5})?';
+		$rexPath     = '(/[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]*?)?';
+		$rexQuery    = '(\?[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
+		$rexFragment = '(#[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
+		$outcome  = "";
+		$validTlds = array_fill_keys(explode(" ", ".aero .asia .biz .cat .com .coop .edu .gov .info .int .jobs .mil .mobi .museum .name .net .org .pro .tel .travel .ac .ad .ae .af .ag .ai .al .am .an .ao .aq .ar .as .at .au .aw .ax .az .ba .bb .bd .be .bf .bg .bh .bi .bj .bm .bn .bo .br .bs .bt .bv .bw .by .bz .ca .cc .cd .cf .cg .ch .ci .ck .cl .cm .cn .co .cr .cu .cv .cx .cy .cz .de .dj .dk .dm .do .dz .ec .ee .eg .er .es .et .eu .fi .fj .fk .fm .fo .fr .ga .gb .gd .ge .gf .gg .gh .gi .gl .gm .gn .gp .gq .gr .gs .gt .gu .gw .gy .hk .hm .hn .hr .ht .hu .id .ie .il .im .in .io .iq .ir .is .it .je .jm .jo .jp .ke .kg .kh .ki .km .kn .kp .kr .kw .ky .kz .la .lb .lc .li .lk .lr .ls .lt .lu .lv .ly .ma .mc .md .me .mg .mh .mk .ml .mm .mn .mo .mp .mq .mr .ms .mt .mu .mv .mw .mx .my .mz .na .nc .ne .nf .ng .ni .nl .no .np .nr .nu .nz .om .pa .pe .pf .pg .ph .pk .pl .pm .pn .pr .ps .pt .pw .py .qa .re .ro .rs .ru .rw .sa .sb .sc .sd .se .sg .sh .si .sj .sk .sl .sm .sn .so .sr .st .su .sv .sy .sz .tc .td .tf .tg .th .tj .tk .tl .tm .tn .to .tp .tr .tt .tv .tw .tz .ua .ug .uk .us .uy .uz .va .vc .ve .vg .vi .vn .vu .wf .ws .ye .yt .yu .za .zm .zw .xn--0zwm56d .xn--11b5bs3a9aj6g .xn--80akhbyknj4f .xn--9t4b11yi5a .xn--deba0ad .xn--g6w251d .xn--hgbk6aj7f53bba .xn--hlcj6aya9esc7a .xn--jxalpdlp .xn--kgbechtv .xn--zckzah .arpa"), true);
+
+		$position = 0;
+		while (preg_match("{\\b$rexProtocol$rexDomain$rexPort$rexPath$rexQuery$rexFragment(?=[?.!,;:\"]?(\s|$))}", $text, $match, PREG_OFFSET_CAPTURE, $position))
+		{
+				list($url, $urlPosition) = $match[0];
+
+				// Print the text leading up to the URL.
+				$outcome .= (htmlspecialchars(substr($text, $position, $urlPosition - $position)));
+
+				$domain = $match[2][0];
+				$port   = $match[3][0];
+				$path   = $match[4][0];
+
+				// Check if the TLD is valid - or that $domain is an IP address.
+				$tld = strtolower(strrchr($domain, '.'));
+				if (preg_match('{\.[0-9]{1,3}}', $tld) || isset($validTlds[$tld]))
+				{
+						// Prepend http:// if no protocol specified
+						$completeUrl = $match[1][0] ? $url : "http://$url";
+
+						// Print the hyperlink.
+						$outcome .= sprintf('<a href="%s">%s</a>', htmlspecialchars($completeUrl), htmlspecialchars("$domain$port$path"));
+				}
+				else
+				{
+						// Not a valid URL.
+						$outcome .= (htmlspecialchars($url));
+				}
+
+				// Continue text parsing from after the URL.
+				$position = $urlPosition + strlen($url);
+		}
+
+		// Print the remainder of the text.
+		$outcome .= ((substr($text, $position)));
+		return $outcome;
 	}
 
 	function getCMSFields() {
