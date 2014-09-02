ebf3b816dac819e789e0062d43e1aac38dd3f03e
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-09-02 00:17:02 +0000

    MINOR
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/sharethis/trunk@5799 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/model/FacebookFeed_Item.php
+++ code/model/FacebookFeed_Item.php
@@ -65,7 +65,7 @@ class FacebookFeed_Item extends DataObject{
 		'KeepOnTop' => true
 	);
 
-	private static $default_sort = "Hide ASC, KeepOnTop DESC, Created DESC";
+	private static $default_sort = "\"Created\" DESC";
 
 	function canCreate($member = null) {
 		return false;
